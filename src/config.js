a9d293d3f705a21309c73751cef2cf140cafc5a7
Michael Worobec <eekumu1@hotmail.com>
2020-12-15 09:48:43 +1100

    fix getBase

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import is from './utils/is';
 
-const getBase = env => `https://${is.empty(env) || env === 'selz.com' ? 'selz.com/sdk' : `${env}/sdk`}/`;
+const getBase = env =>
+    `https://${is.empty(env) || ['selz.com', 'app.selz.com'].includes(env) ? 'app.selz.com/sdk' : `${env}/sdk`}/`;
 
 const config = {
     urls: {
